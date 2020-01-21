179ee669060c93b2f293eeab40405c8a09d5d4fa
Joseph Cho <jscho13@gmail.com>
2020-01-21 18:13:30 -0500

    Add server solution

--- solution.js
+++ solution.js
@@ -5,6 +5,10 @@ const WebSocket = require('ws');
 const server = new WebSocket.Server({ port: 1337 });
 
 server.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('Message from FE: ', message);
+  });
+
   ws.send('ka');
 });
 
